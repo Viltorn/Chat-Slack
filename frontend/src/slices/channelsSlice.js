880977e8c28da0a8d0b561c251059c95d8bec701
Viltorn <viltorn@gmail.com>
2023-02-20 15:45:03 +0300

    try to fix linter

--- frontend/src/slices/channelsSlice.js
+++ frontend/src/slices/channelsSlice.js
@@ -10,29 +10,33 @@ const channelSlice = createSlice({
   initialState,
   reducers: {
     addChannels(state, { payload }) {
-      state.channels = payload;
+      const newChannels = payload;
+      state.channels = newChannels;
     },
     addChannel(state, { payload }) {
       state.channels.push(payload);
     },
     removeChannel(state, { payload }) {
       const { id } = payload;
-      state.channels = state.channels.filter((channel) => channel.id !== id);
+      const newState = state.channels.filter((channel) => channel.id !== id);
+      state.channels = newState;
       if (state.currentChannelId === id) {
         state.currentChannelId = state.channels[0].id || '';
       }
     },
     renameChannel(state, { payload }) {
       const { id, name } = payload;
-      state.channels = state.channels.map((channel) => {
+      const newState = state.channels.map((channel) => {
         if (channel.id === id) {
           channel.name = name;
         }
         return channel;
       });
+      state.channels = newState;
     },
     changeCurrentChannel(state, { payload }) {
-      state.currentChannelId = payload;
+      const id = payload;
+      state.currentChannelId = id;
     },
   },
 });
